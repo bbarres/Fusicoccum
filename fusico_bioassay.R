9d2b86abdca656fb2109c732a37bdc94b1813597
bbarres <benoit.barres@gmail.com>
2019-06-17 18:02:22 +0200

    some improvement to the code

--- fusico_bioassay.R
+++ fusico_bioassay.R
@@ -20,7 +20,7 @@ datafusamy<-read.table("data/fusicodat.txt",header=TRUE,sep="\t")
 #first we extract the list of the different SA listed in the file
 SAlist<-levels(datafusamy$active_substance)
 CompRez<-data.frame(Subs_Act=factor(),sample_ID=factor(),
-                    ED50=character())
+                    ED50=character(),STERR=character())
 #we make a subselection of the data according to the SA
 for (j in 1:length(SAlist)) {
   data_subSA<-datafusamy[datafusamy$active_substance==SAlist[j],]
@@ -32,9 +32,10 @@ for (j in 1:length(SAlist)) {
                                   "strain_ID"])
   ifelse(length(SA_rez)==0,
          REZSA<-data.frame(Subs_Act=factor(),sample_ID=factor(),
-                           ED50=character()),
+                           ED50=character(),STERR=character()),
          REZSA<-data.frame("Subs_Act"=SAlist[j],"sample_ID"=SA_rez,
-                           "ED50"=paste(">",max(data_subSA$dose),sep="")))
+                           "ED50"=paste(">",max(data_subSA$dose),sep=""),
+                           "STERR"="unknown"))
   #we limit the dataset to the sample that reach somehow a IC of 50%
   SA.dat<-data_subSA[!(data_subSA$strain_ID %in% SA_rez),]
   SA.dat<-drop.levels(SA.dat)
@@ -44,11 +45,12 @@ for (j in 1:length(SAlist)) {
                                names(table(SA.dat$strain_ID))[i],],
                  fct=LL.4())
     plot(temp.m1,main=names(table(SA.dat$strain_ID))[i],
-         ylim=c(0,110),xlim=c(0,50))
+         ylim=c(0,120),xlim=c(0,150))
     temp<-ED(temp.m1,50,type="absolute")
     tempx<-data.frame("Subs_Act"=SAlist[j],
                       "sample_ID"=names(table(SA.dat$strain_ID))[i],
-                      "ED50"=as.character(temp[1]))
+                      "ED50"=as.character(temp[1]),
+                      "STERR"=as.character(temp[2]))
     REZSA<-rbind(REZSA,tempx)
   }
   CompRez<-rbind(CompRez,REZSA)
@@ -63,7 +65,7 @@ write.table(CompRez, file="output/results_fusico.txt",
 #first, we replace the ED50 that were too high to be evaluated with the dose 
 #range used with an absurdly high value
 CompRez$ED50<-as.numeric(as.character(CompRez$ED50))
-CompRez[is.na(CompRez$ED50),"ED50"]<-10000
+CompRez[is.na(CompRez$ED50),"ED50"]<-100
 barplot(as.numeric(as.character(CompRez$ED50)),
         ylim=c(0,50),col=CompRez$Subs_Act)
 
