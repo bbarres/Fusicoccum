9927a339c630ea25ca200e0a80cf8251f8b9a20b
bbarres <benoit.barres@gmail.com>
2020-04-21 01:05:36 +0200

    modifying the code for the regression, especially for populations analysis

--- fusico_bioassay.R
+++ fusico_bioassay.R
@@ -28,9 +28,12 @@ for (j in 1:length(SAlist)) {
                                   "strain_ID"])
   ifelse(length(SA_rez)==0,
          REZSA<-data.frame(Subs_Act=factor(),sample_ID=factor(),
-                           ED50=character(),STERR=character()),
+                           ED50=character(),STERR=character(),
+                           ED99.9=character(),STERR=character()),
          REZSA<-data.frame("Subs_Act"=SAlist[j],"sample_ID"=SA_rez,
                            "ED50"=paste(">",max(data_subSA$dose),sep=""),
+                           "STERR"="unknown",
+                           "ED99.9"=paste(">",max(data_subSA$dose),sep=""),
                            "STERR"="unknown"))
   #we limit the dataset to the sample that reach somehow a IC of 50%
   SA.dat<-data_subSA[!(data_subSA$strain_ID %in% SA_rez),]
@@ -39,15 +42,18 @@ for (j in 1:length(SAlist)) {
     temp.m1<-drm(perc_croiss~dose,
                  data=SA.dat[SA.dat$strain_ID==
                                names(table(SA.dat$strain_ID))[i],],
-                 fct=LL.3())
+                 fct=LN.3())
     plot(temp.m1,main=names(table(SA.dat$strain_ID))[i],
          ylim=c(0,120),xlim=c(0,150))
-    temp<-ED(temp.m1,c(50,0.01),type="absolute")
+    temp<-ED(temp.m1,c(50,0.1),type="absolute")
     tempx<-data.frame("Subs_Act"=SAlist[j],
                       "sample_ID"=names(table(SA.dat$strain_ID))[i],
-                      "ED50"=as.character(temp[1]),
-                      "STERR"=as.character(temp[2]))
+                      "ED50"=as.character(temp[1,1]),
+                      "STERR"=as.character(temp[1,2]),
+                      "ED99.9"=as.character(temp[2,1]),
+                      "STERR"=as.character(temp[2,2]))
     REZSA<-rbind(REZSA,tempx)
+    recap.mod<-list(recap.mod,temp.m1)
   }
   CompRezPOP<-rbind(CompRezPOP,REZSA)
   
@@ -57,9 +63,20 @@ for (j in 1:length(SAlist)) {
 write.table(CompRezPOP, file="output/results_fusicoPOP.txt",
             sep="\t",quote=FALSE,row.names=FALSE)
 
-#just a small graphic to gain insight on the first round of results
-#first, we replace the ED50 that were too high to be evaluated with the dose 
-#range used with an absurdly high value
+
+datafusamyPOP<-datafusamy[datafusamy$strain_type=="population",]
+data_subSA<-datafusamyPOP[datafusamyPOP$active_substance=="carbendazim",]
+#simplier way of doing approximately the same thing
+temp.m1<-drm(perc_croiss~dose,
+             data=data_subSA,curveid=strain_ID,
+             fct=LN.3())
+plot(temp.m1,xlim=c(0,150))
+temp<-ED(temp.m1,c(50,0.1),type="absolute")
+
+
+#just a small graphic to gain insight on the first round of results first, 
+#we replace the ED50 that were too high to be evaluated with the dose range 
+#used with an absurdly high value
 CompRezPOP$ED50<-as.numeric(as.character(CompRezPOP$ED50))
 CompRezPOP[is.na(CompRezPOP$ED50),"ED50"]<-100
 barplot(as.numeric(as.character(CompRezPOP$ED50)),
@@ -74,7 +91,7 @@ plot(sort(as.numeric(as.character(
 #Regression analysis of mycelial growth experiment####
 ##############################################################################/
 
-datafusamyIND<-datafusamy[datafusamy$strain_type=="individual",]
+datafusamyIND<-datafusamy[datafusamy$strain_type!="population",]
 #first we extract the list of the different SA listed in the file
 SAlist<-levels(datafusamyIND$active_substance)
 CompRez<-data.frame(Subs_Act=factor(),sample_ID=factor(),
@@ -101,7 +118,7 @@ for (j in 1:length(SAlist)) {
     temp.m1<-drm(perc_croiss~dose,
                  data=SA.dat[SA.dat$strain_ID==
                                names(table(SA.dat$strain_ID))[i],],
-                 fct=LL.4())
+                 fct=LN.3())
     plot(temp.m1,ylim=c(0,120),xlim=c(0,150),
          main=paste(SAlist[j],names(table(SA.dat$strain_ID))[i]),
          col.main=j)
@@ -125,14 +142,12 @@ write.table(CompRez, file="output/results_fusicoIND.txt",
 #range used with an absurdly high value
 CompRez$ED50<-as.numeric(as.character(CompRez$ED50))
 CompRez[is.na(CompRez$ED50),"ED50"]<-100
-barplot(as.numeric(as.character(CompRez$ED50)),
-        ylim=c(0,50),col=CompRez$Subs_Act)
-
-
 plot(sort(as.numeric(as.character(
-  CompRez[CompRez$Subs_Act=="carbendazim",]$ED50))))
+  CompRez[CompRez$Subs_Act=="carbendazim",]$ED50))),
+  log="y",las=1,ylim=c(0.01,100))
 plot(sort(as.numeric(as.character(
-  CompRez[CompRez$Subs_Act=="diethofencarb",]$ED50))))
+  CompRez[CompRez$Subs_Act=="diethofencarb",]$ED50))),
+  log="y",las=1,ylim=c(0.01,100))
 
 
 ##############################################################################/
